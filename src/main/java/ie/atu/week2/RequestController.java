93efa62801557bbe9544529298d84e5a54301d49
G00414253@atu.ie <g00414253@atu.ie>
2024-09-25 15:35:14 +0100

    Final assignment finished

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -3,7 +3,7 @@ package ie.atu.week2;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/person")
+//@RequestMapping("/person")
 public class RequestController {
 @GetMapping("/sayHello")
     public String sayHello(){
@@ -18,5 +18,27 @@ public class RequestController {
     @GetMapping("/details")
     public String details(@RequestParam String Name,@RequestParam int Age){
     return "Name: " + Name + "\n Age: " + Age;
+    }
+
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation) {
+        int Result;
+
+        if (operation.equals("add")) {
+            Result = num1 + num2;
+            return "Num1:" + num1 + "\nNum2: " + num2 + "\nOperation: " + operation + "\nResult:" + Result;
+        }else if (operation.equals("subtract")) {
+            Result = num1 - num2;
+            return "Num1:" + num1 + "\nNum2: " + num2 + "\nOperation: " + operation + "\nResult:" + Result;
+        }else if (operation.equals("multiply")) {
+            Result = num1 * num2;
+            return "Num1:" + num1 + "\nNum2: " + num2 + "\nOperation: " + operation + "\nResult:" + Result;
+        }if (operation.equals("divide")) {
+            Result = num1 / num2;
+            return "Num1:" + num1 + "\nNum2: " + num2 + "\nOperation: " + operation + "\nResult:" + Result;
+        }
+        else{
+            return "Invalid Entry" + operation;
+        }
     }
 }
